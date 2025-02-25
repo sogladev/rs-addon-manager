8dcd51d93711fe0541eceee98fb1345450d208f9
Jelle Meeus <sogladev@gmail.com>
2025-02-25 14:08:43 +0100

    add storing of selected directory with tauri plugin store
    ctrl+c ctrl+v icon from https://techicons.dev/icons/vuejs

--- tauri-game-launcher/src-tauri/src/lib.rs
+++ tauri-game-launcher/src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ struct AppState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .setup(|app| {
             app.manage(Mutex::new(AppState::default()));
             Ok(())
