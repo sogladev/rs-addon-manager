faa947dc6cd1ecf8717ccc65540d47b456c5b508
Jelle Meeus <sogladev@gmail.com>
2025-07-30 21:31:50 +0200

    refactor

--- addon-gui/src-tauri/src/lib.rs
+++ addon-gui/src-tauri/src/lib.rs
@@ -5,8 +5,6 @@ pub mod install;
 pub mod symlink;
 pub mod validate;
 
-use install::install_addon;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -15,7 +13,7 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![
             validate::is_valid_repo_url,
             validate::is_valid_addons_folder_str,
-            install_addon
+            install::install_addon
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
