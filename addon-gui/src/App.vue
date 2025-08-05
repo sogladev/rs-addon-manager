57e1ad6e87d98c5f82ac7da3f7eb809011cfafc7
Jelle Meeus <sogladev@gmail.com>
2025-08-05 18:09:32 +0200

    remove theme

--- addon-gui/src/App.vue
+++ addon-gui/src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
 import AddonManager from '@/views/AddonManager.vue'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-    document.documentElement.setAttribute('data-theme', 'default')
-})
 </script>
 
 <template>
