88cee27716318f62e26628447b1e7ee15cb2b198
Jelle Meeus <sogladev@gmail.com>
2025-09-02 14:46:31 +0200

    remove style

--- addon-manager-gui/src/components/AddonCatalogueModal.vue
+++ addon-manager-gui/src/components/AddonCatalogueModal.vue
@@ -322,13 +322,3 @@ const closeModal = () => {
         </form>
     </dialog>
 </template>
-
-<style scoped>
-.line-clamp-2 {
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    line-clamp: 2;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-}
-</style>
