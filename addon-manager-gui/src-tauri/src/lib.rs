c1cb620213aa8d5f7425c246daaaf8caca79a7eb
Jelle Meeus <sogladev@gmail.com>
2025-09-03 00:09:57 +0200

    add plugin process

--- addon-manager-gui/src-tauri/src/lib.rs
+++ addon-manager-gui/src-tauri/src/lib.rs
@@ -16,6 +16,7 @@ pub mod view_models;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .manage(addon_discovery::AppState::default())
